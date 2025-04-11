812047d0778a6e462a3edee344a7d2913386028a
youwei.cao <gofar@outlook.com>
2025-04-11 17:11:56 +0800

    add tf node for velodyne

--- slam_gmapping/launch/slam_gmapping.launch.py
+++ slam_gmapping/launch/slam_gmapping.launch.py
@@ -5,8 +5,27 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='true')
+    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
     return LaunchDescription([
         launch_ros.actions.Node(
-            package='slam_gmapping', node_executable='slam_gmapping', output='screen', parameters=[{'use_sim_time':use_sim_time}]),
+            package='slam_gmapping', 
+            executable='slam_gmapping', 
+            output='screen', 
+            parameters=[{'use_sim_time':use_sim_time}]
+        ),
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_odom_base_link',
+            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link'],
+            output='screen'
+        ),
+
+        launch_ros.actions.Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_base_link_velodyne',
+            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'velodyne'],
+            output='screen'
+        ),
     ])
